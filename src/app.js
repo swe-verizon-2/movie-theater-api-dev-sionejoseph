1b11116d3529cd41543b78d82c53ce1701f35f39
Sione <dev.sionejoseph@gmail.com>
2024-09-27 15:00:39 -0500

    user routes fixed and fully tested through Postman

--- src/app.js
+++ src/app.js
@@ -6,12 +6,14 @@ const { db } = require("../db/connection");
 const port = 3000;
 
 // middleware 
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use( express.json() );
+app.use( express.urlencoded({extended: true}) );
 
 // routes
 const userRouter = require('./routes/user');
 const showRouter = require('./routes/show');
 
 app.use("/users", userRouter);
-app.use("/shows", showRouter);
+//app.use("/shows", showRouter);
+
+module.exports = app;
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../../models/User');
-const Show = require('../../models/Show');
+const { User, Show } = require('../../models');
 
 router.get("/", async (req, res, next) => {
     try {
@@ -26,22 +25,30 @@ router.get("/:id", async (req, res, next) => {
 router.get("/:id/shows", async (req, res, next) => {
     try {
        const user = await User.findByPk(req.params.id, {include: Show});
-       res.json(user.Shows);
+       if (!(user.shows.length > 0)){
+        res.json({message: "you have not watched any shows"})
+       } else {
+        res.json(user.shows);
+       }
     } catch (e) {
         console.error(e);
         next(e);
     }
 })
 
-router.put("/:id/shows", async (req, res, next) => {
+router.put("/:userId/shows/:showId", async (req, res, next) => {
     try {
-       let user = await User.findByPk(req.params.id);
-       const shows = Show.findAll();
-       await user.addShows(shows[0]);
-       user = await User.findByPk(req.params.id, {include: Show});
-       res.json(user.shows);
+       const user = await User.findByPk(req.params.userId);
+       const show = await Show.findByPk(req.params.showId);
+       await user.addShow(show);
+
+       const updatedUser = await User.findByPk(req.params.userId, {include: Show});
+       res.json(updatedUser.shows);
+
     } catch (e) {
         console.error(e);
         next(e);
     }
 })
+
+module.exports = router;
